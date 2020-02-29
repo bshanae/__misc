5fe08d8ec17a2c656058dbd0d5ec4b4cea66f222
belchenkovova <49374370+belchenkovova@users.noreply.github.com>
2020-02-29 21:44:22 +0300

    QT_Calculator : Writing symbols

--- QT_calculator/main_window.cpp
+++ QT_calculator/main_window.cpp
@@ -1,15 +1,85 @@
 #include "main_window.h"
 #include "ui_main_window.h"
 
-main_window::main_window(QWidget *parent)
-	: QMainWindow(parent)
-	, ui(new Ui::main_window)
+//					PUBLIC
+
+					main_window::main_window(QWidget *parent) :
+					QMainWindow(parent),
+					ui(new Ui::main_window)
 {
 	ui->setupUi(this);
 }
 
-main_window::~main_window()
+					main_window::~main_window()
 {
 	delete ui;
 }
 
+//					SLOTS
+
+void				main_window::on_button_0_clicked()
+{
+	calculator.write_symbol('0');
+	update_text();
+}
+
+void				main_window::on_button_1_clicked()
+{
+	calculator.write_symbol('1');
+	update_text();
+}
+
+void				main_window::on_button_2_clicked()
+{
+	calculator.write_symbol('2');
+	update_text();
+}
+
+void				main_window::on_button_3_clicked()
+{
+	calculator.write_symbol('3');
+	update_text();
+}
+
+void				main_window::on_button_4_clicked()
+{
+	calculator.write_symbol('4');
+	update_text();
+}
+
+void				main_window::on_button_5_clicked()
+{
+	calculator.write_symbol('5');
+	update_text();
+}
+
+void				main_window::on_button_6_clicked()
+{
+	calculator.write_symbol('6');
+	update_text();
+}
+
+void				main_window::on_button_7_clicked()
+{
+	calculator.write_symbol('7');
+	update_text();
+}
+
+void				main_window::on_button_8_clicked()
+{
+	calculator.write_symbol('8');
+	update_text();
+}
+
+void				main_window::on_button_9_clicked()
+{
+	calculator.write_symbol('9');
+	update_text();
+}
+
+//					PRIVATE
+
+void				main_window::update_text()
+{
+	ui->text_browser->setText(calculator.read_line());
+}
--- QT_calculator/main_window.h
+++ QT_calculator/main_window.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_WINDOW_H
 #define MAIN_WINDOW_H
 
+#include "calculator.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -12,10 +14,28 @@ class main_window : public QMainWindow
 	Q_OBJECT
 
 public:
-	main_window(QWidget *parent = nullptr);
-	~main_window();
+					main_window(QWidget *parent = nullptr);
+					~main_window();
+
+private slots:
+
+	void			on_button_0_clicked();
+	void			on_button_1_clicked();
+	void			on_button_2_clicked();
+	void			on_button_3_clicked();
+	void			on_button_4_clicked();
+	void			on_button_5_clicked();
+	void			on_button_6_clicked();
+	void			on_button_7_clicked();
+	void			on_button_8_clicked();
+	void			on_button_9_clicked();
 
 private:
-	Ui::main_window *ui;
+
+	Ui::main_window	*ui;
+	calculator		calculator;
+
+	void			update_text();
 };
-#endif // MAIN_WINDOW_H
+
+#endif
