763396af65eb5de1df715f828c612e0c1f882a3b
belchenkovova <49374370+belchenkovova@users.noreply.github.com>
2020-02-29 18:54:20 +0300

    QT Calculator strated, calculator class

---
+++ QT_calculator/calculator.cpp
@@ -0,0 +1,105 @@
+#include "calculator.h"
+
+//						PUBLIC
+
+						calculator::calculator()
+{
+	clear();
+}
+
+void					calculator::write_symbol(const char &symbol)
+{
+	is_latent_string = false;
+	*current_string += symbol;
+}
+
+void					calculator::write_action(const char &action)
+{
+	is_latent_string = false;
+	if (action == '=')
+		evaluate();
+	else
+	{
+		this->action = action;
+		switch_string();
+	}
+}
+
+void					calculator::write_sign()
+{
+	switch_sign();
+}
+
+void					calculator::clear()
+{
+	left = "";
+	right = "";
+	is_latent_string = true;
+	switch_string(&left);
+	switch_sign(true);
+}
+
+const char				*calculator::read_line()
+{
+	if (is_latent_string)
+		return ("0");
+	else
+		return (current_string->c_str());
+}
+
+//						PRIVATE
+
+void					calculator::switch_string(std::string *ptr)
+{
+	if (ptr)
+	{
+		current_string = ptr;
+		return ;
+	}
+	if (*current_string == left)
+		current_string = &right;
+	else if (*current_string == right)
+		evaluate();
+}
+
+void					calculator::switch_sign(const bool &reset)
+{
+	if (reset)
+	{
+		is_minus = false;
+		return ;
+	}
+	if (is_minus)
+		current_string->erase(current_string->begin());
+	else if (not is_minus)
+		current_string->insert(0, "-");
+	is_minus = not is_minus;
+}
+
+void					calculator::evaluate()
+{
+	float				value_left = std::stof(left);
+	float				value_right = std::stof(right);
+	float				result;
+	std::stringstream	stream;
+	std::string			string;
+
+	result = perform_action(value_left, value_right, action[0]);
+
+	stream << std::fixed << std::setprecision(4) << result;
+	string = stream.str();
+	while (not string.empty())
+		if (string.back() == '0')
+			string.erase(string.size() - 1);
+		else if (string.back() == '.')
+		{
+			string.erase(string.size() - 1);
+			break ;
+		}
+		else
+			break ;
+
+	left = string;
+	right = "";
+	switch_string(&left);
+}
