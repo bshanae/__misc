81dea440af54741d8457c4bb7463f8b9eb264214
belchenkovova <49374370+belchenkovova@users.noreply.github.com>
2020-03-02 14:19:10 +0300

    QT_Text_editor : Mac-like CSS style for button

--- QT_Text_editor/main_window.slots.cpp
+++ QT_Text_editor/main_window.slots.cpp
@@ -16,8 +16,23 @@ void				main_window::on_spin_font_valueChanged(int value)
 	set_font("", "", value);
 }
 
+#define TEMPLATE							\
+"QPushButton								\
+{											\
+	padding : 2px 11px 1px 11px;			\
+	border-top : 1px solid #c8c8c8;			\
+	border-bottom : 1px solid #acacac;		\
+	border-left : 1px solid #c2c2c2;		\
+	border-right : 1px solid #c2c2c2;		\
+	margin : 2px 0px 1px 0px;				\
+	background-color : white;				\
+	border-radius : 4px;					\
+}"
+
 void				main_window::on_button_color_clicked()
 {
 	QColor			new_color = QColorDialog::getColor();
+
+	ui->button_color->setStyleSheet(TEMPLATE);
 }
 
